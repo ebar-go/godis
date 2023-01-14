b9a8406e4a1f9b1cb15741bbeacdb7808c80ddb4
hongker <xiaok2013@live.com>
2023-01-14 21:05:17 +0800

    ListPack

--- server/types/listpack.go
+++ server/types/listpack.go
@@ -1,1 +1,20 @@
 package types
+
+type ListPack struct {
+	size    uint
+	entries []ListPackEntry
+}
+
+type ListPackEntry struct {
+	Value any
+}
+
+func NewListPack(capacity uint) *ListPack {
+	return &ListPack{entries: make([]ListPackEntry, 0, capacity)}
+}
+
+func (lp *ListPack) Insert(val any) {
+	entry := ListPackEntry{Value: val}
+	lp.entries = append(lp.entries, entry)
+	lp.size++
+}
