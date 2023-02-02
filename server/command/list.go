d979b3d6dd0eac5453a61d0c5d9f51a073272f37
hongker <xiaok2013@live.com>
2023-02-02 21:38:02 +0800

    command list

---
+++ server/command/list.go
@@ -0,0 +1,10 @@
+package command
+
+type List interface {
+	LPush(key string, value ...string) error
+	RPush(key string, value ...string) error
+	LPop(key string) (string, error)
+	RPop(key string) (string, error)
+	LRange(key string, start, stop int64) ([]string, error)
+	Len(key string) int
+}
