d34b56be61945663e8302f6e0e1fb2e554104d5e
hongker <xiaok2013@live.com>
2023-02-09 20:53:33 +0800

    add test

---
+++ server_test.go
@@ -0,0 +1,17 @@
+package godis
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewServer(t *testing.T) {
+	srv := NewServer()
+	err := srv.Set("foo", "bar")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := srv.Get("foo")
+	assert.Equal(t, "bar", res)
+}
